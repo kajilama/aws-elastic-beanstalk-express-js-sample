8ac0e3ecbc43f080be896250e4df8c35938b6e6c
kajilama <kazilama@hotmail.com>
2021-04-11 18:34:48 -0400

    Full pipleline test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Good evening all!\nNow running in AWS!'));
+app.get('/', (req, res) => res.send('Good evening all!\nNow running in AWS!\nPart II'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
